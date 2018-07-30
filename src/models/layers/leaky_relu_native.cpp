35e60e9154027b8179862911c3b09fe002d73750
Maarten Vandersteegen <maarten.vandersteegen@kuleuven.be>
2018-07-30 14:52:39 +0200

    Modified native leaky relu layer so it optimizes better
    Functionally, nothing changes, but first scale sublayer can now
    be fused into previous scale or conv layers during network build time

--- src/models/layers/leaky_relu_native.cpp
+++ src/models/layers/leaky_relu_native.cpp
@@ -9,37 +9,40 @@ ILayer* LeakyReluNative::operator()(std::string name, INetworkDefinition* networ
     Weights scales_1{dt, nullptr, 1};
     Weights scales_2{dt, nullptr, 1};
 
+    const float scale1 = (1 - negSlope);
+    const float scale2 = (negSlope / scale1);
+
     if (dt == DataType::kHALF) {
-        m_scale_value_1_h = __float2half(negSlope);
-        m_scale_value_2_h = __float2half(1 - negSlope);
+        m_scale_value_1_h = __float2half(scale1);
+        m_scale_value_2_h = __float2half(scale2);
         scales_1.values = &m_scale_value_1_h;
         scales_2.values = &m_scale_value_2_h;
     } else {
-        m_scale_value_1_f = negSlope;
-        m_scale_value_2_f = 1 - negSlope;
+        m_scale_value_1_f = scale1;
+        m_scale_value_2_f = scale2;
         scales_1.values = &m_scale_value_1_f;
         scales_2.values = &m_scale_value_2_f;
     }
 
-    ILayer* hidden_1 = network->addScale(input, ScaleMode::kUNIFORM, default_weights, scales_1, default_weights);
-    if (!hidden_1)
+    ILayer* layer_scale1 = network->addScale(input, ScaleMode::kUNIFORM, default_weights, scales_1, default_weights);
+    if (!layer_scale1)
         return nullptr;
-    hidden_1->setName(std::string(name + "_leaky_hidden_1").c_str());
+    layer_scale1->setName(std::string(name + "_leaky_scale1").c_str());
 
-    ILayer* hidden_2 = network->addScale(input, ScaleMode::kUNIFORM, default_weights, scales_2, default_weights);
-    if (!hidden_2)
+    ILayer* layer_scale2 = network->addScale(*layer_scale1->getOutput(0), ScaleMode::kUNIFORM, default_weights, scales_2, default_weights);
+    if (!layer_scale2)
         return nullptr;
-    hidden_2->setName(std::string(name + "_leaky_hidden_2").c_str());
+    layer_scale2->setName(std::string(name + "_leaky_scale2").c_str());
 
-    ILayer* hidden_3 = network->addActivation(*hidden_2->getOutput(0), ActivationType::kRELU);
-    if (!hidden_3)
+    ILayer* relu = network->addActivation(*layer_scale1->getOutput(0), ActivationType::kRELU);
+    if (!relu)
         return nullptr;
-    hidden_3->setName(std::string(name + "_leaky_hidden_3").c_str());
+    relu->setName(std::string(name + "_leaky_relu").c_str());
 
-    ILayer* activation = network->addElementWise(*hidden_1->getOutput(0), *hidden_3->getOutput(0), ElementWiseOperation::kSUM);
+    ILayer* activation = network->addElementWise(*layer_scale2->getOutput(0), *relu->getOutput(0), ElementWiseOperation::kSUM);
     if (!activation)
         return nullptr;
-    activation->setName(std::string(name + "_leaky").c_str());
+    activation->setName(std::string(name + "_leaky_add").c_str());
 
     return activation;
 }
--- src/models/layers/leaky_relu_native.h
+++ src/models/layers/leaky_relu_native.h
@@ -12,10 +12,15 @@ class LeakyReluNative : public ILeakyRelu
     /*
      * Building PReLU using native TensorRT layers. Leaky ReLU can be calulated by:
      *
-     * out = neg_slope * in + ReLU(in * (1-neg_slope))
+     *  out = (in * scale1) * scale2 + ReLU((in * scale1))
      *
-     * This requires 2 scale operations (the two multiplications), one ReLU operations and an element wise addition
-     */
+     *  where scale1 = 1 - negSlope
+     *  where scale2 = negSlope / scale1
+     *
+     * This requires 2 scale operations (the two multiplications), one ReLU operations and an element wise addition.
+     * Since optimization can merge scale1 into the previous layer (other scale layer or convolution), this will be
+     * optimized to 1 scale operator, one ReLU and one element wise addition.
+     * */
 public:
     nvinfer1::ILayer* operator()(std::string name, nvinfer1::INetworkDefinition* network, nvinfer1::ITensor& input,
                                float negSlope, nvinfer1::DataType dt);
